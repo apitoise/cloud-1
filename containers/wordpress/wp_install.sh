289c149322bf394b52394d9f3e2d8cc12d2e2147
apitoiset <arthur.pitoiset@webdrone.fr>
2024-03-11 17:15:35 +0100

    clean env var

--- containers/wordpress/wp_install.sh
+++ containers/wordpress/wp_install.sh
@@ -8,8 +8,8 @@ cd /var/www/html
 
 wp core download --locale=fr_FR --allow-root
 
-wp config create --dbname="$WORDPRESS_DB_NAME" --dbuser="$WORDPRESS_DB_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --locale=fr_FR --allow-root
+wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --locale=fr_FR --allow-root
 
-wp core install --url=apitoise.rature.fr --title="Apitoise 42" --admin_user="$WORDPRESS_USER" --admin_password="$WORDPRESS_PASSWORD" --admin_email=admin@example.com --skip-email --allow-root
+wp core install --url=apitoise.rature.fr --title="Apitoise 42" --admin_user=$WORDPRESS_USER --admin_password=$WORDPRESS_PASSWORD --admin_email=admin@example.com --skip-email --allow-root
 
 apache2-foreground
